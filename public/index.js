795aa08a61634ea6f3e248f9251eaf607aa0d297
Swiip <matthieu.lux@gmail.com>
2018-08-24 13:04:35 +0200

    step 1

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
 document.querySelector("body").addEventListener("mouseout", () => {
-  document.querySelector("h1").classList.add("end");
+  document.querySelector("h2.step-1").classList.add("end");
+  document.querySelector("h2.step-2").classList.add("start");
+  document.querySelector("h2.step-3").classList.add("start");
+  document.querySelector("p").classList.add("start");
+  document.querySelector("body").classList.add("start");
 });
