a0765ca4ffc3128cce82f63e7ae8a17876d9e071
lisequesnel <quesnel.lise@gmail.com>
2018-08-24 15:31:25 +0200

    step 2

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/87c0289e", (req, res) => {
-  res.render("step-2", { name: "Swiip" });
+  res.render("step-2");
+});
+
+app.get("/1gf45g8a", (req, res) => {
+  res.render("step-3");
 });
 
 app.listen(1234);
