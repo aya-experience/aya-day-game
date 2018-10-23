3c95c02ae7353655a0f2a7de63c5f2620f01dc8e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-23 09:38:55 +0000

    Deploy puzzle.aya-experience.com to github.com/aya-experience/aya-day-game.git:gh-pages

---
+++ pre-build.sh
@@ -0,0 +1,8 @@
+echo 'Changing view names for the game...'
+
+mv step-2.html 87c0289e.html
+mv step-3-1.html 580e5872.html
+mv step-3-2.html ea8c0a1e.html
+mv step-4.html e69b9e73.html
+mv step-5.html sa87ui24.html
+mv step-6.html 03e463f8.html
